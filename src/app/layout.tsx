3db57c18f1c02a9a58ec46e813bdeda7f0702561
manyadigital-dev <manyadigital.dev@gmail.com>
2026-01-04 23:22:40 -0300

    feat: Add site icons and configure their usage in layout and robots.txt.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,11 @@ export const metadata: Metadata = {
     canonical: './',
   },
   icons: {
-    icon: '/icon.png',
+    icon: [
+      { url: '/icon.png' },
+      { url: '/icon.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: '/icon.png',
     apple: '/apple-icon.png',
   },
 };
